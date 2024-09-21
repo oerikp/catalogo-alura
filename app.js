89b84fac1a820a31b7cb73b1b7ed40b02d3a6769
Seu Nome <seu-email@example.com>
2024-09-20 22:08:51 -0300

    Primeiro commit do projeto

---
+++ app.js
@@ -0,0 +1,6 @@
+function watchMovie(url) {
+    window.location.href = url;
+  }
+console.log(" este site foi criado inteiramente pelo Erik Pereira, pseudônimo dev: epdr.exe")
+console.log(`meu GitHub: https://github.com/oerikp`) 
+alert(`é possivel que nem todas as imagens funcionem corretamente.`) 
